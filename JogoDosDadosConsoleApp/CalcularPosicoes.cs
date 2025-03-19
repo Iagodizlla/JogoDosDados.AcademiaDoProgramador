61fc734a5edd056ceb2dff44f935cc80ed96832c
Iagodizlla <iagoschlemper@gmail.com>
2025-03-19 15:05:52 -0300

    Estruturacao Iniciada

---
+++ JogoDosDadosConsoleApp/CalcularPosicoes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDosDadosConsoleApp
+{
+    internal class CalcularPosicoes
+    {
+        public static int PosicaoUsuario(int PosicaoU, int dado)
+        {
+            PosicaoU += dado;
+            if (PosicaoU == 10)
+            {
+                PosicaoU += 3;
+                Console.WriteLine("Voce achou um taxi e andou mais 3 casas");
+            }
+            else if (PosicaoU == 15)
+            {
+                PosicaoU -= 2;
+                Console.WriteLine("Voce foi roubado e voltou 2 casas");
+            }
+            else if (PosicaoU == 20)
+            {
+                PosicaoU += 2;
+                Console.WriteLine("Voce pegou uma carona e andou mais 2 casas");
+            }
+            else if (PosicaoU == 25)
+            {
+                PosicaoU -= 3;
+                Console.WriteLine("Voce foi sequestrado e voltou 3 casas");
+            }
+            return PosicaoU;
+        }
+        public static int PosicaoRobo(int PosicaoC, int dado)
+        {
+            PosicaoC += dado;
+            if (PosicaoC == 10)
+            {
+                PosicaoC -= 3;
+                Console.WriteLine("O robo foi sequestrado e voltou 3 casas");
+            }
+            else if (PosicaoC == 15)
+            {
+                PosicaoC += 2;
+                Console.WriteLine("O robo pegou uma carona e andou mais 2 casas");
+            }
+            else if (PosicaoC == 20)
+            {
+                PosicaoC -= 2;
+                Console.WriteLine("O robo foi roubado e voltou 2 casas");
+            }
+            else if (PosicaoC == 25)
+            {
+                PosicaoC += 3;
+                Console.WriteLine("O robo achou um taxi e andou mais 3 casas");
+            }
+            return PosicaoC;
+        }
+    }
+}
--- JogoDosDadosConsoleApp/Program.cs
+++ JogoDosDadosConsoleApp/Program.cs
@@ -7,74 +7,22 @@
             int PosicaoU = 0, PosicaoC = 0;
             while (true)
             {
-                Console.Clear();
-                Console.WriteLine("__________________________");
-                Console.WriteLine("Jogo dos Dados");
-                Console.WriteLine("--------------------------\n");
+                Cabecalho();
+                DigitarEnter();
 
-                Console.WriteLine("Digite ENTER para girar o dado");
-                Console.ReadLine();
+                int dado = GerarNumero();
 
-                Random numero = new Random();
-                int dado = numero.Next(1, 7);
+                MenuGirar(dado);
+                PosicaoU = CalcularPosicoes.PosicaoUsuario(PosicaoU, dado);
 
-                Console.WriteLine("__________________________");
-                Console.WriteLine($"Giroooou e caiu: {dado}");
-                Console.WriteLine("--------------------------");
-                PosicaoU += dado;
+                DigitarEnter();
+                dado = GerarNumero();
 
-                if (PosicaoU == 10)
-                {
-                    PosicaoU += 3;
-                    Console.WriteLine("Voce achou um taxi e andou mais 3 casas");
-                }
-                else if (PosicaoU == 15)
-                {
-                    PosicaoU -= 2;
-                    Console.WriteLine("Voce foi roubado e voltou 2 casas");
-                }
-                else if (PosicaoU == 20)
-                {
-                    PosicaoU += 2;
-                    Console.WriteLine("Voce pegou uma carona e andou mais 2 casas");
-                }
-                else if (PosicaoU == 25)
-                {
-                    PosicaoU -= 3;
-                    Console.WriteLine("Voce foi sequestrado e voltou 3 casas");
-                }
+                MenuGirar(dado);
+                PosicaoC = CalcularPosicoes.PosicaoRobo(PosicaoC, dado);
 
-                Console.WriteLine("\nDigite ENTER para o Computador girar o dado");
-                Console.ReadLine();
-                dado = numero.Next(1, 7);
+                MostarPosicao(PosicaoU, PosicaoC);
 
-                Console.WriteLine("__________________________");
-                Console.WriteLine($"Giroooou e caiu: {dado}");
-                Console.WriteLine("--------------------------");
-                PosicaoC += dado;
-
-                if (PosicaoC == 10)
-                {
-                    PosicaoC -= 3;
-                    Console.WriteLine("O robo foi sequestrado e voltou 3 casas");
-                }
-                else if (PosicaoC == 15)
-                {
-                    PosicaoC += 2;
-                    Console.WriteLine("O robo pegou uma carona e andou mais 2 casas");
-                }
-                else if (PosicaoC == 20)
-                {
-                    PosicaoC -= 2;
-                    Console.WriteLine("O robo foi roubado e voltou 2 casas");
-                } else if(PosicaoC == 25)
-                {
-                    PosicaoC += 3;
-                    Console.WriteLine("O robo achou um taxi e andou mais 3 casas");
-                }
-
-                Console.WriteLine($"\nO robo esta na posicao: {PosicaoC}");
-                Console.WriteLine($"Você está na posição: {PosicaoU}");
                 Console.Write("Deseja continuar? (S/N): ");
                 string resposta = Console.ReadLine()!.ToUpper();
 
@@ -83,6 +31,35 @@
                     break;
                 }
             }
+        }
+        static void Cabecalho()
+        {
+            Console.Clear();
+            Console.WriteLine("__________________________");
+            Console.WriteLine("Jogo dos Dados");
+            Console.WriteLine("--------------------------\n");
+        }
+        static int GerarNumero()
+        {
+            Random numero = new Random();
+            int dado = numero.Next(1, 7);
+            return dado;
+        }
+        static void MenuGirar(int dado)
+        {
+            Console.WriteLine("__________________________");
+            Console.WriteLine($"Giroooou e caiu: {dado}");
+            Console.WriteLine("--------------------------");
+        }
+        static void DigitarEnter()
+        {
+            Console.WriteLine("Digite ENTER para girar o dado");
+            Console.ReadLine();
+        }
+        static void MostarPosicao(int PosicaoU, int PosicaoC)
+        {
+            Console.WriteLine($"\nO robo esta na posicao: {PosicaoC}");
+            Console.WriteLine($"Você está na posição: {PosicaoU}");
         }
     }
 }
